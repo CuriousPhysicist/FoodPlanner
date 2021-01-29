3b3bf155c34080c2a67d69e59d9b4132c0613f9a
CuriousPhysicist <andrewhampson6@gmail.com>
2021-01-29 11:22:14 +0000

    Add basic ingredient show page

--- app/controllers/ingredient_controller.rb
+++ app/controllers/ingredient_controller.rb
@@ -2,4 +2,8 @@ class IngredientController < ApplicationController
   def index
     @ingredients = Ingredient.all
   end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'ingredient#index'
 
   get 'test_page/index'
+
+  resources :ingredient, only: %i[index, show]
 end
