205a0b83f8f011f5cc292ea0a6ecdbec518833d7
CuriousPhysicist <andrewhampson6@gmail.com>
2021-02-02 08:32:48 +0000

    Add purgecss and configure to run in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,20 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./app/views/**/*.html.erb"],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss'),
-	require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
+    // needed to purge in production
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 }
